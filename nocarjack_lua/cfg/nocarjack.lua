48fcfc44dbeb7be43acd61b754bd6fb25d129979
Sighmir <guistoppa1995@hotmail.com>
2017-10-19 06:28:11 -0200

    Added config with blacklist + minor improvements
    Added config with blacklist + minor improvements

---
+++ nocarjack_lua/cfg/nocarjack.lua
@@ -0,0 +1,8 @@
+cfg = {}
+
+cfg.chance = 10 -- higher means more dificult to find an unlocked car (10 = 10%, 100 = 1%, 1 = 100%, 2 = 50%, ...)
+
+cfg.blacklist = { -- vehicles that will always be locked when spawned naturally
+  "T20",
+  "RHINO"
+}
--- nocarjack_lua/client.lua
+++ nocarjack_lua/client.lua
@@ -1,21 +1,29 @@
-local chance = 10 -- higher means more dificult to find an unlocked car
-
 Citizen.CreateThread(function()
     while true do
         if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId())) then
             local veh = GetVehiclePedIsTryingToEnter(PlayerPedId())
             local lock = GetVehicleDoorLockStatus(veh)
+			local backlisted = false
+			local lucky = (math.random(1,cfg.chance) == 1)
+			
+			for k,v in pairs(cfg.blacklist) do
+			  if IsVehicleModel(veh, GetHashKey(v)) then
+			    blacklisted = true
+			  end
+			end
 
             if lock == 7 then
-				if math.random(1,chance) ~= 1 then
+				if not lucky or blacklisted then
 					SetVehicleDoorsLocked(veh, 2)
+				else
+					SetVehicleDoorsLocked(veh, 1)
 				end
             end
 
             local pedd = GetPedInVehicleSeat(veh, -1)
 
             if pedd then
-				if math.random(1,chance) ~= 1 then
+				if not lucky or blacklisted then
 					SetPedCanBeDraggedOut(pedd, false)
 				end
             end
