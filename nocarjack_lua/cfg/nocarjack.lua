c61c8ddf013777630030715a9feb1d7822cc3a26
Sighmir <guistoppa1995@hotmail.com>
2017-10-19 14:43:11 -0200

    Improved cfg.chance to make sense and commentaries :smile:
    Improved cfg.chance to make sense and commentaries :smile:

--- nocarjack_lua/cfg/nocarjack.lua
+++ nocarjack_lua/cfg/nocarjack.lua
@@ -1,6 +1,6 @@
 cfg = {}
 
-cfg.chance = 10 -- higher means more dificult to find an unlocked car (10 = 10%, 100 = 1%, 1 = 100%, 2 = 50%, ...)
+cfg.chance = 10 -- chance of being unlocked in percentage
 
 cfg.blacklist = { -- vehicles that will always be locked when spawned naturally
   "T20",
--- nocarjack_lua/client.lua
+++ nocarjack_lua/client.lua
@@ -1,17 +1,30 @@
 Citizen.CreateThread(function()
     while true do
+		-- gets if player is entering vehicle
         if DoesEntityExist(GetVehiclePedIsTryingToEnter(PlayerPedId())) then
+			-- gets vehicle player is trying to enter and its lock status
             local veh = GetVehiclePedIsTryingToEnter(PlayerPedId())
             local lock = GetVehicleDoorLockStatus(veh)
-			local backlisted = false
-			local lucky = (math.random(1,cfg.chance) == 1)
 			
+			-- normalizes chance
+			if cfg.chance > 100 then
+			  cfg.chance = 100
+			elseif cfg.chance < 0 then
+			  cfg.chance = 0
+			end
+			
+			-- check if got lucky
+			local lucky = (math.random(100) < cfg.chance)
+			
+			-- check if vehicle is in blacklist
+			local backlisted = false
 			for k,v in pairs(cfg.blacklist) do
 			  if IsVehicleModel(veh, GetHashKey(v)) then
 			    blacklisted = true
 			  end
 			end
 
+			-- lock doors if not lucky or blacklisted
             if lock == 7 then
 				if not lucky or blacklisted then
 					SetVehicleDoorsLocked(veh, 2)
@@ -20,9 +33,11 @@ Citizen.CreateThread(function()
 				end
             end
 
+			-- gets ped that is driving the vehicle
             local pedd = GetPedInVehicleSeat(veh, -1)
-
+            -- checks if ped exists
             if pedd then
+				-- make ped not draggable if not lucky or blacklisted
 				if not lucky or blacklisted then
 					SetPedCanBeDraggedOut(pedd, false)
 				end
